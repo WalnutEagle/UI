674e26cca0ba085d1ea97be54e90a0620f41accd
Adwait Kulkarni <151489169+WalnutEagle@users.noreply.github.com>
2025-05-24 22:23:15 -0400

    Adding Dynamic resizing and Oh my god it is working is the Restore point I can use

--- components/ImageView.tsx
+++ components/ImageView.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 
 interface ImageViewProps {
@@ -10,7 +9,7 @@ interface ImageViewProps {
 
 export const ImageView: React.FC<ImageViewProps> = ({ src, alt, title, className }) => {
   return (
-    <div className={`relative rounded-lg overflow-hidden bg-gray-700 aspect-video ${className}`}>
+    <div className={`relative w-full rounded-lg overflow-hidden bg-gray-700 aspect-video ${className || ''}`}>
       <img src={src} alt={alt} className="w-full h-full object-cover" />
       {title && (
         <div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 text-white text-xs p-2 text-center">
